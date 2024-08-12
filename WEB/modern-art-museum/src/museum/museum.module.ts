29958f0193e4604abc87435a4049fd412a5a6b50
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2024-08-12 12:16:55 -0500

    commit

---
+++ WEB/modern-art-museum/src/museum/museum.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MuseumService } from './museum.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MuseumEntity } from './museum.entity';
+import { MuseumController } from './museum.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([MuseumEntity])],
+  providers: [MuseumService],
+  controllers: [MuseumController],
+})
+export class MuseumModule {}
